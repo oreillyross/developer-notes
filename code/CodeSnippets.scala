9ec048876f20b763eb09d9f9cb3d96b491b4437e
Ross O'Reilly <oreillyross@gmail.com>
2015-05-31 21:42:31 +0200

    more

--- code/CodeSnippets.scala
+++ code/CodeSnippets.scala
@@ -2,7 +2,6 @@
 
 class ExampleCollections {
 
-  val Spices = Map("Oregano" -> "Italian", "Cardemon" -> "Indian")
   
 
 
@@ -11,7 +10,41 @@ class ExampleCollections {
 
 object ExampleCollections extends App {
 
-  println("Hello")
+ 
+  val spices = Map("Oregano" -> "Italy", "Cardemon" -> "India")
 
+  // foreach on maps
   
+  spices.foreach {
+    case (spice, country) => println(s"$spice is from $country")
+  }
+
+   
+  // one can loop over any Traversable
+  
+  val fruits = List("apples", "pears", "peaches")
+
+  for (f <- fruits) {
+    
+    println(s"$f")
+    val F = f.toUpperCase
+    println(s"$F")
+
+  }
+
+  // use zipWithIndex when you need a loop counteri. use view to create a lazy view on original list
+
+  for ((f, count) <- fruits.view.zipWithIndex) {
+    println(s"$count is an $f")
+  }
+
+ // another way using Streams or a range also works well for a defined range
+ 
+ for ((f, count) <- fruits.zip(Stream from 5)) 
+    println(s"$count is an $f")
+
+
+
+
+
 }
