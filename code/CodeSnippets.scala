5165385502293ab0855466a9241f29fcb8a926f0
Ross O'Reilly <oreillyross@gmail.com>
2015-06-29 12:55:39 +0200

    Update CodeSnippets.scala

--- code/CodeSnippets.scala
+++ code/CodeSnippets.scala
@@ -44,7 +44,19 @@ object ExampleCollections extends App {
     println(s"$count is an $f")
 
 
+// Sorting an existing map by key or value
 
+// given a basic immutable map.
+val grades = Map("Kim" -> 90, "Al" -> 85, "Jim" -> 24, "Emma" -> 45)
+
+// sort the map by key from low to high
+import scala.collection.immutable.ListMap
+
+val x = ListMap(grades.toSeq.sortBy(_._1):_*)
+
+// or use sortwith to sort keys by ascending or descending order
+
+val y = ListMap(grades.toSeq.sortWith(_._1 < _._1): _*) 
 
 
 }
